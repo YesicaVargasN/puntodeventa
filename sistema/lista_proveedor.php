b943136cbafc87e66daef9873bd28bbf6c652e9d
yesicanava <yesica.vargas@tam.gob.mx>
2022-05-25 13:51:33 -0500

    agregar ventas

--- sistema/lista_proveedor.php
+++ sistema/lista_proveedor.php
@@ -41,7 +41,7 @@
 									<td><?php echo $data['direccion']; ?></td>
 									<?php if ($_SESSION['rol'] == 1) { ?>
 									<td>
-										<a href="editar_proveedor.php?id=<?php echo $data['codproveedor']; ?>" class="btn btn-success"><i class='fas fa-edit'></i> Editar</a>
+										<a href="editar_proveedor.php?id=<?php echo $data['codproveedor']; ?>" class="btn btn-success"><i class='fas fa-edit'></i></a>
 										<form action="eliminar_proveedor.php?id=<?php echo $data['codproveedor']; ?>" method="post" class="confirmar d-inline">
 											<button class="btn btn-danger" type="submit"><i class='fas fa-trash-alt'></i> </button>
 										</form>
--- sistema/nueva_venta.php
+++ sistema/nueva_venta.php
@@ -1,113 +1,99 @@
-<?php include_once "includes/header.php"; ?>
-<div class="container-fluid">
-    <div class="row">
-        <div class="col-lg-12">
-            <div class="form-group">
-                <h4 class="text-center">Datos del Cliente</h4>
-                <a href="#" class="btn btn-primary btn_new_cliente"><i class="fas fa-user-plus"></i> Nuevo Cliente</a>
-            </div>
-            <div class="card">
-                <div class="card-body">
-                    <form method="post" name="form_new_cliente_venta" id="form_new_cliente_venta">
-                        <input type="hidden" name="action" value="addCliente">
-                        <input type="hidden" id="idcliente" value="1" name="idcliente" required>
-                        <div class="row">
-                            <div class="col-lg-4">
-                                <div class="form-group">
-                                    <label>Dni</label>
-                                    <input type="number" name="dni_cliente" id="dni_cliente" class="form-control">
-                                </div>
-                            </div>
-                            <div class="col-lg-4">
-                                <div class="form-group">
-                                    <label>Nombre</label>
-                                    <input type="text" name="nom_cliente" id="nom_cliente" class="form-control" disabled required>
-                                </div>
-                            </div>
-                            <div class="col-lg-4">
-                                <div class="form-group">
-                                    <label>Teléfono</label>
-                                    <input type="number" name="tel_cliente" id="tel_cliente" class="form-control" disabled required>
-                                </div>
-                            </div>
-                            <div class="col-lg-4">
-                                <div class="form-group">
-                                    <label>Dirreción</label>
-                                    <input type="text" name="dir_cliente" id="dir_cliente" class="form-control" disabled required>
-                                </div>
+<?php 
+session_start();
+include_once "includes/header.php"; 
+if(!isset($_SESSION["carrito"])) $_SESSION["carrito"] = [];
+$granTotal = 0;
+?>
 
-                            </div>
-                            <div id="div_registro_cliente" style="display: none;">
-                                <button type="submit" class="btn btn-primary">Guardar</button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-            <h4 class="text-center">Datos Venta</h4>
-            <div class="row">
-                <div class="col-lg-6">
-                    <div class="form-group">
-                        <label><i class="fas fa-user"></i> VENDEDOR</label>
-                        <p style="font-size: 16px; text-transform: uppercase; color: red;"><?php echo $_SESSION['nombre']; ?></p>
-                    </div>
-                </div>
-                <div class="col-lg-6">
-                    <label>Acciones</label>
-                    <div id="acciones_venta" class="form-group">
-                        <a href="#" class="btn btn-danger" id="btn_anular_venta">Anular</a>
-                        <a href="#" class="btn btn-primary" id="btn_facturar_venta"><i class="fas fa-save"></i> Generar Venta</a>
-                    </div>
-                </div>
-            </div>
-            <div class="table-responsive">
-                <table class="table table-hover">
-                    <thead class="thead-dark">
-                        <tr>
-                            <th width="100px">Código</th>
-                            <th>Des.</th>
-                            <th>Stock</th>
-                            <th width="100px">Cantidad</th>
-                            <th class="textright">Precio</th>
-                            <th class="textright">Precio Total</th>
-                            <th>Acciones</th>
-                        </tr>
-                        <tr>
-                            <td><input type="hidden" name="txt_cod_producto" id="txt_cod_producto">
-                                <input type="text" name="txt_cod_pro" id="txt_cod_pro">
-                            </td>
-                            <td id="txt_descripcion">-</td>
-                            <td id="txt_existencia">-</td>
-                            <td><input type="text" name="txt_cant_producto" id="txt_cant_producto" value="0" min="1" disabled></td>
-                            <td id="txt_precio" class="textright">0.00</td>
-                            <td id="txt_precio_total" class="txtright">0.00</td>
-                            <td><a href="#" id="add_product_venta" class="btn btn-dark" style="display: none;">Agregar</a></td>
-                        </tr>
-                        <tr>
-                            <th>Id</th>
-                            <th colspan="2">Descripción</th>
-                            <th>Cantidad</th>
-                            <th class="textright">Precio</th>
-                            <th class="textright">Precio Total</th>
-                            <th>Acciones</th>
-                        </tr>
-                    </thead>
-                    <tbody id="detalle_venta">
-                        <!-- Contenido ajax -->
+<div class="col-xs-12">
+		<?php
+			if(isset($_GET["status"])){
+				if($_GET["status"] === "1"){
+					?>
+						<div class="alert alert-success">
+							<strong>¡Correcto!</strong> Venta realizada correctamente
+						</div>
+					<?php
+				}else if($_GET["status"] === "2"){
+					?>
+					<div class="alert alert-info">
+							<strong>Venta cancelada</strong>
+						</div>
+					<?php
+				}else if($_GET["status"] === "3"){
+					?>
+					<div class="alert alert-info">
+							<strong>Ok</strong> Producto quitado de la lista
+						</div>
+					<?php
+				}else if($_GET["status"] === "4"){
+					?>
+					<div class="alert alert-warning">
+							<strong>Error:</strong> El producto que buscas no existe
+						</div>
+					<?php
+				}else if($_GET["status"] === "5"){
+					?>
+					<div class="alert alert-danger">
+							<strong>Error: </strong>El producto está agotado
+						</div>
+					<?php
+				}else{
+					?>
+					<div class="alert alert-danger">
+							<strong>Error:</strong> Algo salió mal mientras se realizaba la venta
+						</div>
+					<?php
+				}
+			}
+		?>
+		<br>
+		<form method="post" action="agregarAlCarrito.php">
+			<label for="codigo">Código de barras:</label>
+			<input autocomplete="off" autofocus class="form-control" name="codigo" required type="text" id="codigo" placeholder="Escribe el código">
+		</form>
+		<br><br>
+		<div class="table-responsive">
+		<table class="table table-bordered  table-hover">
+			<thead class="thead-dark">
+				<tr>
+					<th>ID</th>
+					<th>Código</th>
+					<th>Descripción</th>
+					<th>Precio de venta</th>
+					<th>Cantidad</th>
+					<th>Total</th>
+					<th>Quitar</th>
+				</tr>
+			</thead>
+			<tbody>
+           
+				<?php
+                 
+                 foreach($_SESSION["carrito"] as $indice => $producto){ 
+						$granTotal += $producto['total'] ;
+					?>
 
-                    </tbody>
 
-                    <tfoot id="detalle_totales">
-                        <!-- Contenido ajax -->
-                    </tfoot>
-                </table>
-
-            </div>
-        </div>
-    </div>
-
-</div>
-<!-- /.container-fluid -->
-
+				<tr>
+					<td><?php echo $producto['id']  ?></td>
+					<td><?php echo $producto['codigo'] ?></td>
+					<td><?php echo $producto['descripcion'] ?></td>
+					<td><?php echo $producto['precioventa'] ?></td>
+					<td><?php echo $producto['cantidad'] ?></td>
+					<td><?php echo $producto['total'] ?></td>
+					<td><a class="btn btn-danger" href="<?php echo "quitarDelCarrito.php?indice=" . $indice?>"><i class="fa fa-trash"></i></a></td>
+				</tr>
+				<?php } ?>
+			</tbody>
+		</table>
+		</div>
+		<h3>Total: <?php echo $granTotal; ?></h3>
+		<form action="./terminarVenta.php" method="POST">
+			<input name="total" type="hidden" value="<?php echo $granTotal;?>">
+			<button type="submit" class="btn btn-success">Terminar venta</button>
+			<a href="./cancelar_venta.php" class="btn btn-danger">Cancelar venta</a>
+		</form>
+	</div>
 
 <?php include_once "includes/footer.php"; ?>
