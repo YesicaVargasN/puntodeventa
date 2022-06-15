8a05a8fe91a81fcc5863bd54e286182d6eefee73
FlorItavu <flor.perezp@tam.gob.mx>
2022-06-15 15:51:25 -0500

    gastos

--- sistema/eliminar_gastos.php
+++ sistema/eliminar_gastos.php
@@ -4,6 +4,6 @@ if (!empty($_GET['id'])) {
     $id = $_GET['id'];
     $query_delete = mysqli_query($conexion, "DELETE FROM gastos WHERE id = $id");
     mysqli_close($conexion);
-    header("location: lista_codigos.php");
+    header("location: lista_factura.php");
 }
 ?>
