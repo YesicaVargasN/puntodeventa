be84b11ff87ccafbe89a9b1e8af626716727729a
FlorItavu <flor.perezp@tam.gob.mx>
2022-06-27 10:45:22 -0500

    eliminados con modal de éxito e historia

--- sistema/eliminar_gastos.php
+++ sistema/eliminar_gastos.php
@@ -1,9 +1,13 @@
+<?php include_once "includes/header.php"; ?>
 <?php
 if (!empty($_GET['id'])) {
     require("../conexion.php");
     $id = $_GET['id'];
     $query_delete = mysqli_query($conexion, "DELETE FROM gastos WHERE id = $id");
+    historia('Se elimino un gasto(factura) '.$id);
     mysqli_close($conexion);
-    header("location: lista_factura.php");
+    mensajeicono('Se ha eliminado con éxito el gasto!', 'lista_factura.php','','exito');
+    //header("location: lista_factura.php");
 }
 ?>
+<?php include_once "includes/footer.php"; ?>
